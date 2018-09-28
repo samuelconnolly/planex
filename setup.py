bcbf4cff2cf6c81e1da385a9f8153bbe00ca7fc3
Sam Connolly <samuel.connolly@citrix.com>
2018-09-28 15:15:03 +0100

    CP-29543 set version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ python-setuptools definition for planex
 from setuptools import setup
 
 setup(name='planex',
-      version='4.0.0',
+      version='4.12b',
       packages=['planex', 'planex.cmd'],
       package_data={'planex': ['Makefile.rules']},
       entry_points={
